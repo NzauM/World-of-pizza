e439efff883554353286e0bcd7adfbc662409381
NzauM <mercywaenu16@gmail.com>
2019-10-06 09:00:28 +0300

    Add functions for price depending on crust and size

--- JS/scripts.js
+++ JS/scripts.js
@@ -9,3 +9,24 @@ function Delivery(name,location){
     this.location = deliveryLOcation;
 };
 
+var pricePerSize = {
+    small: 650,
+    medium: 800,
+    large:1000,
+};
+
+var pricePerCrust = {
+    crispy: 150,
+    stuffed:200,
+    gluttenFree:200,
+},
+
+var toppingsperSize = [
+    {
+        pineapple={
+            small:50,
+            medium:100,
+            large:150,
+        }
+    }
+]
