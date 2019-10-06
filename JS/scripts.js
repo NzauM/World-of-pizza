2685c6a359fd9a320aacbf83f8edd3415f443267
NzauM <mercywaenu16@gmail.com>
2019-10-06 08:51:06 +0300

    add constructors for pizza and delivery location

---
+++ JS/scripts.js
@@ -0,0 +1,11 @@
+function Pizza(size,crust){
+    this.size = pizzaSize;
+    this.crust = pizzaCrut;
+    this.toppings = [];
+};
+
+function Delivery(name,location){
+    this.name = name;
+    this.location = deliveryLOcation;
+};
+
