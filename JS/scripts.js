aaecb5cfc64adc274a5747c25cf59156970a913f
NzauM <mercywaenu16@gmail.com>
2019-10-06 09:12:04 +0300

    Add function to calculate price according to size

--- JS/scripts.js
+++ JS/scripts.js
@@ -27,6 +27,39 @@ var toppingsperSize = [
             small:50,
             medium:100,
             large:150,
-        }
+        },
+
+        pepperoni={
+            small:100,
+            medium:150,
+            large:200,
+        },
+
+        mushroom={
+            small:80,
+            medium:120,
+            large:150,
+        },
+
+        chicken={
+            small:150,
+            medium:200,
+            large:250,
+        },
+    },
+];
+
+// Start Calculator
+function getSizePrice(size){
+    if (size === 'small'){
+        return pricePerSize.small * 1;
     }
-]
+    else if(size === 'medium'){
+        return pricePerSize.medium * 1;
+    }
+
+    else {
+        return pricePerSize.large * 1;
+    }
+}
+
